f271d51701e53ebc1129c6e3fd571b22c1762df1
Austin Melms <memyselfandlie@gmail.com>
2018-03-12 02:36:22 -0400

    greentext renamed to 4chan

--- src/commands/funCommands/4chan.js
+++ src/commands/funCommands/4chan.js
@@ -1,8 +1,8 @@
 const { GenericRedditCommand } = require('../../models')
 
 module.exports = new GenericRedditCommand({
-  triggers: ['greentext', 'gt'],
-  description: '>sending greentexts',
+  triggers: ['4chan'],
+  description: 'Yes these 4chan posts come from reddit. Get over it, reddit is better.',
 
   endpoint: '/r/greentext/top/.json?sort=top&t=day&limit=400',
   type: 'image'
