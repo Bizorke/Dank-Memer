80339fd3a8d30bd92e4b39ad26aa666040de0f50
Devoxin <lukel9914@gmail.com>
2018-03-12 23:13:34 +0000

    FML command

---
+++ src/commands/funCommands/fml.js
@@ -0,0 +1,37 @@
+const { GenericCommand } = require('../../models/')
+const { get } = require('snekfetch')
+const cheerio = require('cheerio')
+
+const indexes = {} // TODO: Move to a better place?
+
+module.exports = new GenericCommand(
+  async ({ msg, addCD }) => {
+    await addCD()
+
+    if (!indexes[msg.channel.guild.id] || indexes[msg.channel.guild.id].length === 0) {
+      const data = await get('http://www.fmylife.com/random')
+
+      if (!data || data.status !== 200) {
+        return 'FML, couldn\'t find anything'
+      }
+
+      const foundStories = []
+      const parser = await cheerio.load(data.text)
+      parser('p.block a')
+        .filter((i, story) => story.children.length > 0 && story.children[0].data.trim().length > 1)
+        .map((i, story) => foundStories.push(story.children[0].data.trim()))
+
+      indexes[msg.channel.guild.id] = foundStories
+    }
+
+    const story = indexes[msg.channel.guild.id].shift()
+
+    return {
+      description: story
+    }
+  }, {
+    triggers: ['fml'],
+    description: 'Think you\'re having a bad day?',
+    cooldown: 3000
+  }
+)
