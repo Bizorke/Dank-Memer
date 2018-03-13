598195c65e84b3a83ea8a2c1cdd558d45757e967
Austin Melms <memyselfandlie@gmail.com>
2018-03-13 16:42:24 -0400

    oh no premium is about to get a big test

--- src/commands/tagSystem/addtag.js
+++ src/commands/tagSystem/addtag.js
@@ -3,6 +3,9 @@ const { GenericCommand } = require('../../models/')
 module.exports = new GenericCommand(
   async ({ Memer, msg, args }) => {
     try {
+      if (!Memer.bot.getChannel(msg.channel.id).permissionsOf(msg.author.id).has('banMembers')) {
+        return 'Only people with ban perms can add tags.'
+      }
       if (!args[1]) {
         return 'You have the wrong syntax for this command.\nExample: pls addtag henlo u stink\nYou: pls tag henlo\nDank Memer: u stink'
       }
