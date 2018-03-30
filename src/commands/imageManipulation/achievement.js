cb81c0bb15d98607311c7e1ba93b246a522bba07
Austin Melms <memyselfandlie@gmail.com>
2018-03-30 01:45:33 -0400

    minecraft command

---
+++ src/commands/imageManipulation/achievement.js
@@ -0,0 +1,13 @@
+const { GenericCommand } = require('../../models/')
+module.exports = new GenericCommand(
+  async ({msg, args}) => {
+    let num = Math.floor(Math.random() * 20) + 1
+    return { image:
+      { url: `https://www.minecraftskinstealer.com/achievement/a.php?i=${num}&h=Achievement+get%21&t=${args.join('+')}` }
+    }
+  },
+  {
+    triggers: ['achievement', 'minecraft', 'mc'],
+    description: 'Am I the only one that didnt know minecraft had achievements'
+  }
+)
