7867e07aee1daa9a138a9c1757f0675c8fab2c22
Austin Melms <memyselfandlie@gmail.com>
2018-04-09 03:30:54 -0400

    updated help command + a space

--- src/commands/utilityCommands/help.js
+++ src/commands/utilityCommands/help.js
@@ -18,7 +18,7 @@ module.exports = new GenericCommand(
 
       return {
         title: 'Available Commands',
-        description: 'We want to be able to upgrade our server for better performance. If you\'re able and willing to donate, you can do so on [patreon](https://www.patreon.com/dankmemerbot) or [paypal](https://www.paypal.me/melmsie). It will help out a ton, and all money goes towards the bot!',
+        description: 'We want to be able to upgrade our server for better performance. If you\'re able and willing to donate, you can do so on [patreon](https://www.patreon.com/dankmemerbot) or [paypal](https://www.paypal.me/melmsie). It will help out a ton, and all money goes towards the bot! (Not to mention you get `1k coins per dollar` donated to help fuel your gambling addictions)',
         fields: Object.keys(categories).map(category => ({ name: category, value: categories[category].join(', ') })),
         footer: { text: 'twitter.com/dankmemerbot' }
       }
--- src/utils/misc.js
+++ src/utils/misc.js
@@ -46,12 +46,12 @@ module.exports = {
       { name: 'm', count: 60 },
       { name: 's', count: 1 }
     ]
-  
+
     const timeStr = [ Math.floor(time / methods[0].count).toString() + methods[0].name ]
     for (let i = 0; i < 3; i++) {
       timeStr.push(Math.floor(time % methods[i].count / methods[i + 1].count).toString() + methods[i + 1].name)
     }
-  
+
     return timeStr.filter(g => !g.startsWith('0')).join(', ')
   },
 
