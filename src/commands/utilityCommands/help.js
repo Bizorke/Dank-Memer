187688ef84fb55efff77eecb1206b010c6c653b5
Austin Melms <memyselfandlie@gmail.com>
2018-04-27 01:46:50 -0400

    help redesign

--- src/commands/utilityCommands/help.js
+++ src/commands/utilityCommands/help.js
@@ -2,6 +2,9 @@ const { GenericCommand } = require('../../models')
 
 module.exports = new GenericCommand(
   async ({ Memer, msg, args, addCD }) => {
+    const db = await Memer.db.getGuild(msg.channel.guild.id)
+    const prefix = db ? db.prefix : Memer.config.defaultPrefix
+    let help = Memer.config.help
     if (!args[0]) {
       let categories = {}
       for (const command of Memer.cmds) {
@@ -17,28 +20,47 @@ module.exports = new GenericCommand(
       }
 
       return {
-        title: 'Available Commands',
-        description: 'Want 750 meme coins? Spend 1 minute [voting here](https://discordbots.org/bot/270904126974590976/vote) and it will get automatically added to your account!\nThis can be ran every 24 hours!',
-        fields: Object.keys(categories).map(category => ({ name: category, value: categories[category].join(', ') })),
-        footer: { text: 'twitter.com/dankmemerbot' }
+        title: help.title,
+        description: help.message,
+        fields: Object.keys(categories).map(category => ({ name: category, value: `${categories[category].length} commands\n\`${prefix} help ${category.split(' ')[1].toLowerCase()}\``, inline: true })),
+        footer: { text: help.footer }
       }
     }
 
     const command = Memer.cmds.find(c => c.props.triggers.includes(args[0]))
-    if (!command) {
-      return
-    }
+    const categorySearch = Memer.cmds.find(c => c.category.split(' ')[1].toLowerCase().includes(args[0]))
 
-    const db = await Memer.db.getGuild(msg.channel.guild.id)
-    const prefix = db ? db.prefix : Memer.config.defaultPrefix
+    if (command) {
+      await addCD()
+      Memer.log(command)
+      return {
+        fields: [
+          { 'name': 'Description:', 'value': command.props.description },
+          { 'name': 'Usage:', 'value': `\`${command.props.usage.replace('{command}', `${prefix} ${command.props.triggers[0]}`)}\`` },
+          { 'name': 'Aliases:', 'value': command.props.triggers.join(', ') }
+        ]
+      }
+    } else if (categorySearch) {
+      await addCD()
+      let categories = {}
+      for (const command of Memer.cmds) {
+        if (command.props.ownerOnly || command.props.hide) {
+          continue
+        }
 
-    await addCD()
-    return {
-      fields: [
-        { 'name': 'Description:', 'value': command.props.description },
-        { 'name': 'Usage:', 'value': Memer.codeblock(command.props.usage.replace('{command}', `${prefix} ${command.props.triggers[0]}`)) },
-        { 'name': 'Triggers:', 'value': command.props.triggers.join(', ') }
-      ]
+        let category = categories[command.category]
+        if (!category) {
+          category = categories[command.category] = []
+        }
+        category.push(command.props.triggers[0])
+      }
+      const categoryName = Object.keys(categories).find(c => c.split(' ')[1].toLowerCase() === args[0])
+      const commands = categories[categoryName]
+      return {
+        title: categoryName,
+        description: commands.join(', '),
+        footer: { text: `use ${prefix} before each command!` }
+      }
     }
   },
   {
