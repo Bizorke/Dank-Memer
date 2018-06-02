8b18c38406ad46b9932f075635cebb038ee75834
Luke <15076404+Devoxin@users.noreply.github.com>
2018-06-02 18:11:54 +0100

    Set authorization header, remove todo

--- src/models/GenericImageCommand.js
+++ src/models/GenericImageCommand.js
@@ -5,7 +5,7 @@ class GenericImageCommand {
   constructor (commandProps, URLParseFN) {
     this.cmdProps = commandProps
     this.URLParseFN = URLParseFN || this.defaultURLParseFN
-    this.requestURL = commandProps.reqURL || `http://127.0.0.1/api/${commandProps.triggers[0]}`
+    this.requestURL = commandProps.reqURL || 'http://127.0.0.1/api/$ENDPOINT'
   }
 
   async run ({ Memer, msg, addCD }) {
@@ -16,8 +16,8 @@ class GenericImageCommand {
 
     const data = await get(this.requestURL.replace('$ENDPOINT', this.cmdProps.triggers[0]))
       .query(datasrc)
+      .set('Authorization', Memer.config.imgenKey)
 
-    // TODO: Check response content-type, set Authorization header
     if (data.status === 200 && data.headers['content-type'].startsWith('image/')) {
       await addCD()
       msg.channel.createMessage('', { file: data.body, name: `${this.cmdProps.triggers[0]}.${this.cmdProps.format || 'png'}` })
