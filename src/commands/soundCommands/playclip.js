a299f0c40bb53b7094c5e2e856875953bccb72fe
Luke <15076404+Devoxin@users.noreply.github.com>
2018-06-10 01:18:50 +0100

    Update playclip.js

--- src/commands/soundCommands/playclip.js
+++ src/commands/soundCommands/playclip.js
@@ -3,6 +3,6 @@ const { GenericSoundboardCommand } = require('../../models/')
 module.exports = new GenericSoundboardCommand({
   triggers: ['playclip'],
   description: 'Plays a custom sound clip',
-  usage: '{command} <clipname> <file attachment>',
+  usage: '{command} <clipname>',
   existingConn: 'Develop the technology to speak two different things simultaneously and then we\'ll talk.'
 })
