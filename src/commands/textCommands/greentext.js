0c4c7bbe71ad7478c7ccd8a2e88292459fb2c8cc
Perry Fraser <perry@frasers.org>
2018-07-01 18:53:35 -0400

    Cleanup in greentext.js

--- src/commands/textCommands/greentext.js
+++ src/commands/textCommands/greentext.js
@@ -1,9 +1,7 @@
 const { GenericCommand } = require('../../models/')
 
 module.exports = new GenericCommand(
-  async ({ cleanArgs }) => {
-    return '```css\n>' + cleanArgs.join(' ') + '\n```'
-  }, {
+  async ({ cleanArgs }) => '```css\n>' + cleanArgs.join(' ') + '\n```', {
     triggers: ['greentext', '>', 'gt'],
     description: 'Make the bot say whatever you want with greentext',
     usage: '{command} <what you want the bot to say>',
