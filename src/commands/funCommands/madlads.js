68c54c79f1e6292fff7224e740fdc0274cce8947
Perry Fraser <perry@frasers.org>
2018-07-01 21:43:18 -0400

    change madlads range to monthly for hopefully better content

--- src/commands/funCommands/madlads.js
+++ src/commands/funCommands/madlads.js
@@ -4,6 +4,6 @@ module.exports = new GenericRedditCommand({
   triggers: ['madlads', 'madlad', 'cantbestopped', '2mad4me', 'madwoman', 'madwomen'],
   description: 'THESE LADS ARE OFF THEIR WALKERS',
 
-  endpoint: '/r/madlads/top/.json?sort=top&t=week&limit=100',
+  endpoint: '/r/madlads/top/.json?sort=top&t=day&limit=100',
   type: 'image'
 })
