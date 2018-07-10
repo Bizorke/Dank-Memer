4fe09abfe6fa46e966ce4ac20a1e036da7ae21d8
Devoxin <lukel9914@gmail.com>
2018-07-10 16:44:17 +0100

    Use TextChannel#purge instead

--- src/commands/modCommands/clean.js
+++ src/commands/modCommands/clean.js
@@ -1,28 +1,5 @@
 const { GenericCommand } = require('../../models/')
 
-function isNewerThan2Weeks (msg) {
-  return msg.timestamp > Date.now() - 14 * 24 * 60 * 60 * 1000
-}
-
-async function cleanBoi (Memer, msg, amount, messages) {
-  messages.length = amount
-
-  if (messages.length === 0) {
-    return `whaddya want me to delete?`
-  }
-
-  const result = await msg.channel.deleteMessages(messages.map(m => m.id))
-    .catch(err => err.message)
-
-  if (result) {
-    return `Something went wrong while deleting the messages\n\`\`\`\n${result}\`\`\``
-  }
-
-  const success = await msg.channel.createMessage(`Deleted ${messages.length} messages. Are ya happy now?`)
-  await Memer.sleep(1500)
-  return success.delete()
-}
-
 module.exports = new GenericCommand(
   async ({ Memer, msg, addCD }) => {
     const perms = msg.channel.permissionsOf(msg.author.id)
@@ -32,26 +9,38 @@ module.exports = new GenericCommand(
 
     await addCD()
 
-    const messages = await msg.channel.getMessages(100)
-    const maxPurge = Math.min(messages.length, 100)
-    const purgeAmount = Math.min(Math.max(Number(msg.args.getArgument(0)) || 10, 0), maxPurge)
+    const purgeAmount = Math.min(Math.max(msg.args.getArgument(0) || 10, 1), 100)
+    let filter = null
 
     switch (msg.args.nextArgument()) {
       case 'bot':
       case 'bots': // clean messages from any bots
-        return cleanBoi(Memer, msg, purgeAmount, messages.filter(m => isNewerThan2Weeks(m) && m.author.bot))
+        filter = (m) => m.author.bot
+        break
 
       case 'user':
       case 'users': // clean messages from all users (or those specified) - excludes bots
         const senders = msg.args.resolveUsers()
         if (senders.length > 0) {
-          return cleanBoi(Memer, msg, purgeAmount, messages.filter(m => isNewerThan2Weeks(m) && senders.some(user => user.id === m.author.id)))
+          filter = (m) => senders.some(user => user.id === m.author.id)
         } else {
-          return cleanBoi(Memer, msg, purgeAmount, messages.filter(m => isNewerThan2Weeks(m) && !m.author.bot))
+          filter = (m) => !m.author.bot
         }
+        break
 
       default: // No arguments/matches, default to cleaning Memer's messages
-        return cleanBoi(Memer, msg, purgeAmount, messages.filter(m => isNewerThan2Weeks(m) && m.author.id === Memer.bot.user.id))
+        filter = (m) => m.author.id === Memer.bot.user.id
+    }
+
+    const deleted = await msg.channel.purge(purgeAmount, filter)
+      .catch(err => err.message)
+
+    if (typeof (deleted) === 'string') {
+      return `Something went wrong while deleting the messages\n\`\`\`\n${deleted}\`\`\``
+    } else {
+      const success = await msg.channel.createMessage(`Deleted ${deleted} messages. Are ya happy now?`)
+      await Memer.sleep(1500)
+      return success.delete()
     }
   },
   {
