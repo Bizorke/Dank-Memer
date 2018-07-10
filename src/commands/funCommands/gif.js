a8b12ad4052b8706df925ccc84e4fe42b294a38e
Austin Melms <memyselfandlie@gmail.com>
2018-07-10 15:39:50 -0400

    gif command update
    needed some changes to comply with better ratelimits

--- src/commands/funCommands/gif.js
+++ src/commands/funCommands/gif.js
@@ -6,8 +6,9 @@ module.exports = new GenericCommand(
     let search = encodeURIComponent(msg.args.args.join(' '))
     const data = await get(`https://api.giphy.com/v1/gifs/search?api_key=${Memer.config.gif}&q=${search}&limit=25&offset=0&rating=PG-13&lang=en`)
     return {
-      title: `${decodeURIComponent(search)} gif`,
-      description: 'Powered by [Giphy](https://giphy.com/)',
+      title: `First Result for "${decodeURIComponent(search)}" on GIPHY`,
+      // description: 'Powered by [GIPHY](https://giphy.com/)',
+      thumbnail: { url: 'https://image.ibb.co/b0Gkwo/Poweredby_640px_Black_Vert_Text.png' },
       image: { url: data.body.data[0].images.original.url }
     }
   }, {
