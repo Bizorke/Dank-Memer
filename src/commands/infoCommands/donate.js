7e2ba856083d352648ea20fb9c5770dcfd675410
Austin Melms <memyselfandlie@gmail.com>
2018-07-13 17:55:39 -0400

    updated donate command

--- src/commands/infoCommands/donate.js
+++ src/commands/infoCommands/donate.js
@@ -3,8 +3,8 @@ const { GenericCommand } = require('../../models/')
 module.exports = new GenericCommand(
   async () => ({
     title: 'Donate to Dank Memer',
-    description: 'Help fund Dank Memer to keep it alive and performing well, as well as earning some sweet perks!\n\n[Paypal](https://www.paypal.me/melmsie) - One time donation\n[Patreon](https://www.patreon.com/dankmemerbot) - Monthly support\n\nIf you donate please remember to join [this server](https://discord.gg/ngy5hz9) or you will not be able to get perks for donating.',
-    footer: { text: 'hi mom!' }
+    description: 'Help fund Dank Memer to keep it alive and performing well, as well as earning some exclusive perks!\n\n[Patreon](https://www.patreon.com/dankmemerbot) - Monthly support\n\nIf you donate please remember to join [this server](https://discord.gg/ngy5hz9) and add Melmsie#0001 as a friend. Then send Melmsie a DM with your patreon name, and he will get you sorted out!',
+    footer: { text: 'idk what to put here' }
   }), {
     triggers: ['donate', 'patreon', 'donut'],
     description: 'See how you can donate to the bot to support the development and get some sweet perks!',
