7330a5fdd1a9573bc0f4168e6dbc007fdcd6b3d3
Austin Melms <memyselfandlie@gmail.com>
2018-07-24 00:00:20 -0400

    mel

--- src/commands/infoCommands/donate.js
+++ src/commands/infoCommands/donate.js
@@ -3,7 +3,7 @@ const { GenericCommand } = require('../../models/')
 module.exports = new GenericCommand(
   async () => ({
     title: 'Donate to Dank Memer',
-    description: 'Help fund Dank Memer to keep it alive and performing well, as well as earning some exclusive perks!\n\n[Patreon](https://www.patreon.com/dankmemerbot) - Monthly support\n\nIf you donate please remember to join [this server](https://discord.gg/ngy5hz9) and add Melmsie#0001 as a friend. Then send Melmsie a DM with your patreon name, and he will get you sorted out!',
+    description: 'Help fund Dank Memer to keep it alive and performing well, as well as earning some exclusive perks!\n\n[Patreon](https://www.patreon.com/dankmemerbot) - Monthly support\n\nIf you donate please remember to join [this server](https://discord.gg/ngy5hz9) and add Mel#0004 as a friend. Then send Mel a DM with your patreon name, and he will get you sorted out!',
     footer: { text: 'idk what to put here' }
   }), {
     triggers: ['donate', 'patreon', 'donut'],
