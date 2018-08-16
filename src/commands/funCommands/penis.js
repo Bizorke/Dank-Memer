5678a3dc5511cf7b63aaa3f3f48bfeb382080d38
Perry Fraser <me@pprogs.blog>
2018-08-16 22:48:48 +0000

    More small pickle improvments

--- src/commands/funCommands/penis.js
+++ src/commands/funCommands/penis.js
@@ -6,11 +6,11 @@ module.exports = new GenericCommand(
     const name = target.nick || target.username
     return {
       title: 'peepee size machine',
-      description: `${name}'s penis\n8${'='.repeat((Math.floor(Math.random() * 10) + 1) * config.devs.includes(target.id) ? 0 : 1)}D`
+      description: `${name}'s penis\n8${'='.repeat((Math.floor(Math.random() * 10) + 1) * ~~!config.devs.includes(target.id))}D`
     }
   },
   {
     triggers: ['penis', 'howbig', 'peepee', 'pickle'],
-    description: 'See how dank you are'
+    description: 'See how cool you are'
   }
 )
