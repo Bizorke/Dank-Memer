b3668022f6e3a7ff9689fa432edc28897515f75e
Blake <bsward8@gmail.com>
2018-08-19 17:21:38 +1000

    Minor fixes and changes to `snipe`

--- src/commands/modCommands/snipe.js
+++ src/commands/modCommands/snipe.js
@@ -14,9 +14,18 @@ module.exports = new GenericModerationCommand(
       return 'There\'s nothing to snipe!'
     }
     await addCD()
+    let content = Memer.snipe[msg.channel.guild.id][channel.id].content
     let user = Memer.bot.users.get(Memer.snipe[msg.channel.guild.id][channel.id].userID)
-    return { author: { name: `${user.username}#${user.discriminator}`, icon_url: user.dynamicAvatarURL() },
-      description: Memer.snipe[msg.channel.guild.id][channel.id].content }
+    return {
+      author:
+        { name: `${user.username}#${user.discriminator}`,
+          icon_url: user.dynamicAvatarURL()
+        },
+      description: content.length > 1024 ? `${content.slice(0, 1020)}...` : content,
+      footer: {
+        text: `User ID: ${user.id}`
+      }
+    }
   },
   {
     triggers: ['snipe', 'sniper'],
