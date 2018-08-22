120a4e070278159222bd9ad43a70123e9d4fba69
Unknown <paradoxalcorp@gmail.com>
2018-08-22 23:40:12 +0200

    *oops forgot that*

--- src/handlers/messageDelete.js
+++ src/handlers/messageDelete.js
@@ -1,5 +1,5 @@
 exports.handle = async function (msg) {
-  if (!msg.content || msg.embeds[0]) {
+  if (!msg.content) {
     return
   }
   this.redis.setAsync(`${msg.channel.guild.id}-${msg.channel.id}`, JSON.stringify({ userID: msg.author.id, content: msg.content, timestamp: msg.timestamp }), 'EX', 60 * 60)
