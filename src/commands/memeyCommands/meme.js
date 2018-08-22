94c6f2a8d1fa5564c495ab356ad5defb26af441c
Unknown <paradoxalcorp@gmail.com>
2018-08-22 22:55:52 +0200

    Update meme's command endpoint

--- src/commands/memeyCommands/meme.js
+++ src/commands/memeyCommands/meme.js
@@ -4,6 +4,6 @@ module.exports = new GenericRedditCommand({
   triggers: ['meme', 'maymay', 'meemee'],
   description: 'See the top new memes on reddit!',
 
-  endpoint: '/u/kerdaloo/m/dankmemer/top/.json?sort=top&t=day&limit=500',
+  endpoint: '/user/kerdaloo/m/dankmemer/top/.json?sort=top&t=day&limit=500',
   type: 'image'
 })
