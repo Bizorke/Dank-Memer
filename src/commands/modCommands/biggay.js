b33bfa01ccd373a5b674381368c7cca21428b26e
Blake <bsward8@gmail.com>
2018-08-24 00:18:03 +1000

    Changes to permission and error handling and code cleanup

--- src/commands/modCommands/biggay.js
+++ src/commands/modCommands/biggay.js
@@ -2,32 +2,46 @@ const { GenericCommand } = require('../../models/')
 
 module.exports = new GenericCommand(
   async ({ Memer, msg, args }) => {
-    const biggay = msg.channel.guild.roles.find(r => r.name === 'big gay')
+    let biggay = msg.channel.guild.roles.find(r => r.name === 'big gay')
     const reason = `\`biggay\` - ran by ${msg.author.username}#${msg.author.discriminator}`
     let member = Memer.randomInArray(msg.channel.guild.members.filter(member => member.id !== Memer.bot.user.id && member.id !== msg.channel.guild.ownerID && (biggay ? !member.roles.includes(biggay.id) : true)))
     if (!member) {
       return 'There\'s nobody that I can give the big gay to!'
     }
-    if (biggay) {
-      member.addRole(biggay.id, reason)
-    } else {
-      msg.channel.guild.createRole({ name: 'big gay', hoist: true, color: '16056407' }, reason)
-        .then(async (role) => {
-          await Memer.sleep(100)
-          let botRole = msg.channel.guild.members.get(Memer.bot.user.id).roles.sort((a, b) => a.position - b.position)[0] || msg.channel.guild.id
-          botRole = msg.channel.guild.roles.get(botRole)
-          await role.editPosition(botRole.position - 1)
-          await member.addRole(role.id, reason)
+
+    const memberHighestRole = member.roles.map(r => msg.channel.guild.roles.get(r)).sort((a, b) => a.position - b.position)[0] || msg.channel.guild.roles.get(msg.channel.guild.id)
+    const botRole = msg.channel.guild.members.get(Memer.bot.user.id).roles.map(r => msg.channel.guild.roles.get(r)).sort((a, b) => a.position - b.position)[0] || msg.channel.guild.roles.get(msg.channel.guild.id)
+    const hahayes = `${member.user.username}#${member.user.discriminator}`
+    if (memberHighestRole.position >= botRole.position) {
+      return `I can't give ${hahayes} the big gay because they have an equal or higher role compared to me!`
+    }
+
+    const addBigGay = (role) => {
+      member.addRole(role.id, reason)
+        .then(() => {
+          msg.channel.createMessage(`${hahayes} now has the **big gay**`)
         })
         .catch(() => {
-          return `I wasn't able to add the big gay role to ${member}. Check that I have the correct permissions to add and edit roles and try again.`
+          msg.channel.createMessage(`I wasn't able to add the big gay role to **${hahayes}**. Make sure that they don't have a higher role than me, and that I have the correct permissions to add roles to people (\`Manage Roles\`)`)
         })
     }
-    return `${member.user.username}#${member.user.discriminator} now has the **big gay**`
+
+    if (!biggay) {
+      msg.channel.guild.createRole({ name: 'big gay', hoist: true, color: '16056407' }, reason)
+        .then(async (role) => {
+          await role.editPosition(botRole.position - 1)
+          addBigGay(role)
+        })
+        .catch(() => {
+          msg.channel.createMessage(`I wasn't able to add the big gay role to **${hahayes}**. Check that I have the correct permissions to add and edit roles and try again.`)
+        })
+    } else {
+      addBigGay(biggay)
+    }
   }, {
     triggers: ['biggay'],
-    usage: '{command} <id> <shit>',
-    description: 'melmsie stinks',
+    usage: '{command}',
+    description: 'Give somebody the big gay',
     perms: ['manageRoles'],
     modPerms: ['manageRoles']
   }
