cdafada39c17d42d6da9d38c6f51e2826b758a32
Blake <bsward8@gmail.com>
2018-08-28 00:05:25 +1000

    Rename command and add DM message on second payment failure

--- src/commands/utilityCommands/dev/commands/expireddonors.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  help: 'Wipe expired donors',
-  fn: async ({ Memer, args }) => {
-    await Memer.db.wipeExpiredDonors()
-    return 'I\'ve gotten rid of those expired/declined donors'
-  }
-}
---
+++ src/commands/utilityCommands/dev/commands/wipedonors.js
@@ -0,0 +1,25 @@
+module.exports = {
+  help: 'Wipe expired donors',
+  fn: async ({ Memer, msg }) => {
+    let donors = await Memer.db.wipeExpiredDonors()
+
+    let promises = []
+    for (let user of donors) {
+      const channel = await Memer.bot.getDMChannel(user.id)
+      promises.push(
+        await channel.createMessage({ embed: {
+          color: 12000284,
+          title: 'Your donor status has been removed',
+          description: 'We were unable to process your payment more than once, and sadly we\'ve had to remove your donor perks.\n\n' +
+          'If you feel that this was a mistake, please update and validate your payment information and pledge the tier you previously had. You\'ll be back where you were in no time!\n\n' +
+          'We appreciate your donations and time, thank you very much!\n' +
+          '-- The Dank Memer Team'
+        }})
+      )
+    }
+
+    await Promise.all(promises).then(async () => {
+      msg.channel.createMessage(`I've successfully messaged ${promises.length} users about expired payments`)
+    })
+  }
+}
