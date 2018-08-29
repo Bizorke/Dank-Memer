afb611b6d6b9f21408dc1aef855898a733754807
Austin Melms <memyselfandlie@gmail.com>
2018-08-29 01:42:31 -0400

    gif emote support

--- src/commands/utilityCommands/randomemoji.js
+++ src/commands/utilityCommands/randomemoji.js
@@ -6,7 +6,7 @@ module.exports = new GenericCommand(
     return {
       title: `Here is your random emoji`,
       description: `⚠ **Disclaimer** ⚠\nThis emoji is from a random server.\nIf it is breaking [Discord TOS](https://discordapp.com/terms) please report it to \`abuse@discordapp.com\` with this info:\nServer ID: ${data.server}\nEmoji ID: ${data.emoji}`,
-      image: { url: `https://cdn.discordapp.com/emojis/${data.emoji}.png?` }
+      image: { url: `https://cdn.discordapp.com/emojis/${data.emoji}.${data.animated ? 'gif' : 'png'}` }
     }
   }, {
     triggers: ['randomemoji', 'randemoji'],
@@ -24,6 +24,7 @@ async function getEmoji (Memer) {
   let chosen = Memer.randomInArray(guild.emojis)
   return {
     server: guild.id,
-    emoji: chosen.id
+    emoji: chosen.id,
+    animated: chosen.animated
   }
 }
