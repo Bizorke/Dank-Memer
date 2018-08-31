71d0f8a08d94094c14de9a820bc9fe4128642a4f
Devoxin <lukel9914@gmail.com>
2018-08-31 16:30:44 +0100

    Fix missing required module

--- src/utils/audioUtils.js
+++ src/utils/audioUtils.js
@@ -1,5 +1,6 @@
 const { get } = require('snekfetch')
 const { constants, promises: fs } = require('fs')
+const { exec } = require('child_process')
 
 async function isOpus (url) {
   const res = await get(url)
