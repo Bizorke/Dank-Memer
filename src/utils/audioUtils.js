4f8d3410e4a31f08a585dece2f5e300073090e0e
Devoxin <lukel9914@gmail.com>
2018-08-31 16:30:03 +0100

    Bring back convertAndSave

--- src/utils/audioUtils.js
+++ src/utils/audioUtils.js
@@ -39,6 +39,21 @@ function exists (path) {
     .catch(() => false)
 }
 
+function convertAndSave (url, dir, file) {
+  return new Promise(async (resolve, reject) => { // Opus is nice and efficient. This should sound like 96K MP3 while consuming less space
+    await makeDir(dir)
+     exec(`ffmpeg -loglevel error -i ${url} -acodec libopus -b:a 96k "${dir}/${file}"`, (err, _, stderr) => {
+      if (err) {
+        return reject(err.message)
+      }
+       if (stderr) {
+        return reject(stderr)
+      }
+       resolve()
+    })
+  })
+}
+
 async function saveAudioData (url, dir, file) {
   const res = await get(url)
   await writeBuffer(dir, file, res.body)
@@ -72,6 +87,7 @@ async function removeFile (path) {
 }
 
 module.exports = {
+  convertAndSave,
   exists,
   getFiles,
   getFileSize,
