d9d43a6e412a31e9ba2d32f6aa2859ad8e5bb882
Unknown <paradoxalcorp@gmail.com>
2018-09-01 01:32:15 +0200

    Make massnick reset restore old nicknames if mass-nicked recently enough
    -The old nickname of the members are now cached in redis for 6 hours when `massnick` is ran, allowing `massnick reset` to restore them 
    -Changed the required permission from `manageGuild` to `manageNicknames` because that's the actually needed permission
    -Made mass-nicking faster by checking if Memer can actually change the nickname of the member (if memer has a higher role), so no more unnecessary calls to the API that would slow it down

--- src/commands/modCommands/massnick.js
+++ src/commands/modCommands/massnick.js
@@ -13,13 +13,15 @@ module.exports = new GenericModerationCommand(
         return 'Ok I guess we\'re not renaming anyone then'
       }
     }
-
+    let oldNicknames
     if (nickname.toLowerCase() === 'reset') {
       nickname = ''
+      oldNicknames = await Memer.redis.getAsync(`massnick-${msg.channel.guild.id}`).then(res => res ? JSON.parse(res) : {})
     }
 
     await addCD()
-    let members = msg.channel.guild.members.filter(m => m.nick || m.user.username !== nickname)
+    let members = msg.channel.guild.members.filter(m => (m.nick || m.user.username !== nickname) &&
+    getHighestRolePos(msg.channel.guild.members.get(Memer.bot.user.id), msg.channel.guild) > getHighestRolePos(m, msg.channel.guild))
     const next = Number(500 * members.length)
     const hours = Math.floor(next / 3600000)
     const minutes = Math.floor((next / 60000) - (hours * 60))
@@ -35,15 +37,26 @@ module.exports = new GenericModerationCommand(
     msg.channel.createMessage(`Now starting to mass nickname all members to **${nickname || 'their username'}**\n**ETA**: ${timeLeft}`)
     const promises = []
     let failed = 0
+    let nicknames = {}
     for (const member of members) {
-      promises.push(
-        member.edit({ nick: nickname }).catch(() => {
-          failed++
-        })
-      )
+      if (nickname && member.nick) {
+        nicknames[member.id] = member.nick
+      }
+      if (getHighestRolePos(msg.channel.guild.members.get(Memer.bot.user.id), msg.channel.guild) > getHighestRolePos(member, msg.channel.guild)) {
+        promises.push(
+          member.edit({ nick: nickname ? nickname : (oldNicknames[member.id] || nickname) }).catch(() => { // eslint-disable-line no-unneeded-ternary
+            failed++
+          })
+        )
+      } else failed++
     }
 
     await Promise.all(promises)
+    if (nickname) {
+      await Memer.redis.setAsync(`massnick-${msg.channel.guild.id}`, JSON.stringify(nicknames), 'EX', 60 * 60 * 6)
+    } else {
+      await Memer.redis.delAsync(`massnick-${msg.channel.guild.id}`)
+    }
     msg.channel.createMessage(`Finished renaming ${members.length - failed} people to ${!nickname ? 'their stinky username' : `**${nickname}**`}.`)
     if (failed) {
       return `I failed to rename ${failed} people, possibly due to permissions.`
@@ -54,6 +67,10 @@ module.exports = new GenericModerationCommand(
     cooldown: 72e5,
     usage: '{command} [nickname | reset]',
     description: 'Warning, this will rename everyone on the server (or everyone with a specific role when provided) if the bot has the correct permissions',
-    modPerms: ['manageGuild']
+    modPerms: ['manageNicknames']
   }
 )
+
+function getHighestRolePos (member, guild) {
+  return member.roles[0] ? guild.roles.filter(r => member.roles.includes(r.id)).sort((a, b) => b.position - a.position)[0].position : 0
+}
