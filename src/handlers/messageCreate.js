51094314495285fada3829cea0723c130f36fc3e
Blake <bsward8@gmail.com>
2018-09-01 22:36:35 +1000

    Improve RegExp

--- src/handlers/messageCreate.js
+++ src/handlers/messageCreate.js
@@ -72,7 +72,7 @@ exports.handle = async function (msg) {
   if (gConfig.swearFilter) {
     let swears = ['fuck', 'penis', 'cunt', 'faggot', 'wank', 'nigger', 'nigga', 'slut', 'bastard', 'bitch', 'asshole', 'dick', 'blowjob', 'cock',
       'pussy', 'retard']
-    let re = new RegExp('(\b|\s|^|.|\,|\ )' + swears.join('|') + '(\b|\s|$|.|\,|\ )', 'i') // eslint-disable-line no-useless-escape
+    let re = new RegExp(`.*(${swears.join('|')}).*`, 'i') // eslint-disable-line no-useless-escape
     const match = re.exec(msg.content)
     if (match) {
       msg.delete()
