75fdb90a75f88587fddb20e3db81ae620d295d66
Blake <bsward8@gmail.com>
2018-09-02 22:31:54 +1000

    Add command to create tags
    This command doesn't make the tag name and content lowercase like the donor bot did

---
+++ src/commands/textCommands/addtag.js
@@ -0,0 +1,27 @@
+const { GenericCommand } = require('../../models/')
+
+module.exports = new GenericCommand(
+  async ({ Memer, msg }) => {
+    if (!Memer.bot.getChannel(msg.channel.id).permissionsOf(msg.author.id).has('banMembers')) {
+      return 'Only people with `Ban Members` can add tags.'
+    }
+    let [ tagName, ...tagText ] = msg.args.args
+    if (!tagName) {
+      return 'You need to give a name for this new tag smh'
+    }
+    if (!tagText) {
+      return 'You have the wrong syntax for this command.\nExample: `pls addtag henlo u stink`\nYou: pls tag henlo\nDank Memer: u stink'
+    } 
+    let allTags = await Memer.db.getAllTags(msg.channel.guild.id)
+    let two = allTags.filter(tag => tag.name === tagName)
+    if (two[0]) {
+      return 'There is already a tag with that name'
+    }
+    Memer.db.addTag(msg.channel.guild.id, tagName, tagText.join(' '))
+    return `Successfully created tag **${tagName}**`
+  }, {
+    triggers: ['addtag', 'createtag'],
+    description: 'Creates a new tag',
+    usage: '{command} [tag name] [content]'
+  }
+)
