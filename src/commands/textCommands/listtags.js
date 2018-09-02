98f72e9c3088cd58a005c4acf00bb64dce1297ae
Blake <bsward8@gmail.com>
2018-09-02 23:36:25 +1000

    Error handling in tag listing

--- src/commands/textCommands/listtags.js
+++ src/commands/textCommands/listtags.js
@@ -4,7 +4,7 @@ module.exports = new GenericCommand(
   async ({ Memer, msg }) => {
     let allTags = await Memer.db.getAllTags(msg.channel.guild.id)
     let mappedTags = allTags.map(tag => `**\`${tag.name}\`**`)
-    if (!mappedTags) {
+    if (!mappedTags.length) {
       return 'This server has no tags!'
     }
     return `Here is a list of all of the tags that are on **${msg.channel.guild.name}**:\n` +
