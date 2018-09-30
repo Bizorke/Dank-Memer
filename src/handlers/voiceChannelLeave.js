81ca5eed254119a1ebb13a7a82efaca34abab0bb
Austin Melms <memyselfandlie@gmail.com>
2018-09-30 18:05:14 -0400

    linter pls

--- src/handlers/voiceChannelLeave.js
+++ src/handlers/voiceChannelLeave.js
@@ -1,8 +1,8 @@
 exports.handle = async function (member, oldChannel) {
   if (member) {
-    const music = this.musicManager.get(member.guild.id)
+    const music = this.musicManager.get(member.guild.id);
     if (music.sfxautoplay.enabled && music.sfxautoplay.host === member) {
-      await music.endSession()
+      await music.endSession();
     }
   }
-}
+};
