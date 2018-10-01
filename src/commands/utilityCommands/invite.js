56508bb504a6a668b6acbaa6662e81ec4250fda9
Austin Melms <memyselfandlie@gmail.com>
2018-10-01 12:24:59 -0400

    Update invite.js

--- src/commands/utilityCommands/invite.js
+++ src/commands/utilityCommands/invite.js
@@ -6,8 +6,8 @@ module.exports = new GenericCommand(
       { name: 'Add Dank Memer', value: '\n[Here](https://goo.gl/HL3zBi)', inline: true },
       { name: 'Dank Memer Support', value: '\n[Here](https://discord.gg/Wejhbd4)', inline: true },
       { name: 'Dank Memer Twitter', value: '\n[Here](https://twitter.com/dankmemerbot)', inline: true },
-      { name: 'Meme Server', value: '\n[Here](https://www.reddit.com/r/dankmemer/)', inline: true },
-      { name: 'Buy some Dank Premium', value: '\n[Here](https://www.patreon.com/dankmemerbot)', inline: true },
+      { name: 'Reddit Page', value: '\n[Here](https://www.reddit.com/r/dankmemer/)', inline: true },
+      { name: 'Buy some Dank Premium', value: '\n[Here](https://www.patreon.com/bePatron?c=362724)', inline: true },
       { name: 'See some Dank Code', value: '\n[Here](https://github.com/Dank-Memer)', inline: true }
 
     ]
